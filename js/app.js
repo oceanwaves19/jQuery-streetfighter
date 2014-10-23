24232ca06cf43776f9f5f55286f18a1676f6599a
oceanwaves19 <Khadija@Khadijas-MacBook-Pro.local>
2014-10-23 15:38:18 -0400

    final part

--- js/app.js
+++ js/app.js
@@ -8,23 +8,44 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
     .mousedown(function() {
-      	playHadouken();  
+      playHadouken();  	 
 	    $('.ryu-ready').hide();
 	    $('.ryu-throwing').show();
 	    $('.hadouken').finish().show()
-  .animate(
-    {'left': '300px'},
-    500,
-    function() {
+
+      .animate(
+      {'left': '300px'},
+      500,
+      function() {
       $(this).hide();
       $(this).css('left', '-199px');
     }
-  )
+  );
   	})
  .mouseup(function() {
   $('.ryu-throwing').hide();
   $('.ryu-ready').show();
-});
+})
+
+
+  $('body').keydown(function(e) {
+      if(e.which == 88) {
+        $('.ryu-throwing').hide();
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();
+      }
+  })
+
+    $('body').keyup(function(e) {
+      if(e.which == 88) {
+        $('.ryu-throwing').hide();
+        $('.ryu-still').show();
+        $('.ryu-cool').hide();
+        $('.ryu-ready').hide();
+     }
+   })
+
 });
 
 function playHadouken () {
